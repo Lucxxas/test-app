bde535b804d93e7abd16f34ecc24720599911477
Lucxxas <159765280+Lucxxas@users.noreply.github.com>
2025-06-06 12:20:45 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,11 @@
 output "instance_ips" {
   value = aws_instance.your_instance_name.public_ip
 }
+
+output "key_ssm_param" {
+  value = aws_ssm_parameter.private_key.name
+}
+
+output "public_ip" {
+  value = aws_instance.TP-FINAL-WEB.public_ip
+}
