ae8ea54549e97b9e1269d391c0fc9ef88be8e097
Lucxxas <159765280+Lucxxas@users.noreply.github.com>
2025-06-06 12:22:15 +0200

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  # Les clés sont gérées par Spacelift via les variables d'environnement
+}
