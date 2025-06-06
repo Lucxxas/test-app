011b8e2cb13b917b913c52d450704960e6aff0f0
Lucxxas <159765280+Lucxxas@users.noreply.github.com>
2025-06-06 14:32:33 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,19 @@
 
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "TP-FINAL"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+  default     = "Password123!"
+}
